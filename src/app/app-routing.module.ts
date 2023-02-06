cf66982d413b01c423fc7a630aeb6d5465912b9e
vaibhav-frontend <124563536+vaibhav-frontend@users.noreply.github.com>
2023-02-06 00:56:27 -0800

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AllStudentsComponent } from './all-students/all-students.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AllStudentsComponent,
+  },
+  {
+    path:'add-student',
+    component: AddStudentComponent
+  },
+  {
+    path: 'edit-student/:id',
+    component: EditStudentComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/student.ts
@@ -0,0 +1,8 @@
+export interface Student {
+    id: number;
+    firstName: string;
+    lastName: string;
+    gender: string;
+    age: number;
+  }
+  
